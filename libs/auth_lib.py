5e1aecd19c88168c18fc47fd3c3ed57a661aa0a7
Alexis Schwindt <alexis.schwindt@outlook.com>
2025-04-28 13:36:29 -0300

    Initial commit with an example of incidents data added to a csv file.

---
+++ libs/auth_lib.py
@@ -0,0 +1,10 @@
+import auth_data.auth_data as auth
+
+def get_instance_url():
+    return auth.instance_url
+
+def get_username():
+    return auth.username
+
+def get_password():
+    return auth.password
---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import csv
+import libs.auth_lib as auth_lib
+
+instance_url = auth_lib.get_instance_url()
+username = auth_lib.get_username()
+password = auth_lib.get_password()
+
+response = requests.get(
+    f"{instance_url}/api/now/table/incident",
+    auth=(username, password)
+)
+
+csv_file = open("./incidents.csv", "w")
+csv_writer = csv.writer(csv_file)
+c = 0
+
+for emp in response.json()['result']:
+    if c == 0:
+
+        # Writing headers of CSV file
+        h = emp.keys()
+        csv_writer.writerow(h)
+        c += 1
+
+    # Writing data of CSV file
+    csv_writer.writerow(emp.values())
+
+csv_file.close()
